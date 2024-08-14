6568d017263001900b5cc11480082c49f9553f8c
Eunsoo Justin Shin <shinej@uchicago.edu>
2024-08-15 06:53:21 +0900

    a better csheme loading system

--- init.lua
+++ init.lua
@@ -783,17 +783,17 @@ require('lazy').setup({
     end,
   },
 
-  -- {
-  --   'sainnhe/sonokai',
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     -- Optionally configure and load the colorscheme
-  --     -- directly inside the plugin declaration.
-  --     vim.g.sonokai_enable_italic = true
-  --     vim.cmd.colorscheme 'sonokai'
-  --   end,
-  -- },
+  {
+    'sainnhe/sonokai',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.sonokai_enable_italic = true
+      -- vim.cmd.colorscheme 'sonokai'
+    end,
+  },
 
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
